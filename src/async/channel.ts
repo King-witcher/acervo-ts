f327b9c64297c675997229590ac15b17718ff313
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-12-18 22:44:09 -0300

    improve channel

--- src/async/channel.ts
+++ src/async/channel.ts
@@ -1,9 +1,10 @@
-import { defer } from '@/utils/utils'
+import { delay, yieldExecution } from '@/utils/utils'
+import { Semaphore } from './semaphore2'
 
 type Receiver<T> = (value: T) => void
 type Sender<T> = {
     value: T
-    resolve: () => void
+    semaphore: Semaphore
 }
 
 /**
@@ -26,16 +27,20 @@ export class Channel<T> {
      * Otherwise, the data is queued until a receiver is available.
      */
     async send(data: T): Promise<void> {
-        return new Promise((resolve) => {
-            const receiver = this.receivers.shift()
-            if (receiver)
-                // Defers the call to make the behavior deterministic
-                defer(() => {
-                    receiver(data)
-                    resolve()
-                })
-            else this.senders.push({ value: data, resolve })
-        })
+        const receiver = this.receivers.shift()
+        if (receiver) {
+            // Immediately deliver data to the waiting receiver
+            receiver(data)
+        } else {
+            // Queue the data until a receiver is available
+            this.senders.push({
+                value: data,
+                semaphore: new Semaphore(),
+            })
+        }
+
+        // Yields execution so that the receiver can run.
+        await yieldExecution()
     }
 
     /**
@@ -53,14 +58,17 @@ export class Channel<T> {
      * Otherwise, the receiver waits until data is sent to the channel.
      */
     async receive(): Promise<T> {
-        return new Promise<T>((resolve) => {
-            const sender = this.senders.shift()
-            if (sender) {
-                sender.resolve()
-                resolve(sender.value)
-            } else {
+        const sender = this.senders.shift()
+        if (sender) {
+            // If data is immediately available, return it synchronously
+            // Signal to the sender that the data has been received
+            sender.semaphore.signal()
+            return sender.value
+        } else {
+            // If no data is available, enqueue as a receiver and wait
+            return new Promise<T>((resolve) => {
                 this.receivers.push(resolve)
-            }
-        })
+            })
+        }
     }
 }
