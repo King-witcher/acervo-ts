b69f45a43bf1c25bcd8217620b4f8d284e3d7196
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2025-12-18 22:28:51 -0300

    improve bounded-channel

--- src/async/bounded-channel.ts
+++ src/async/bounded-channel.ts
@@ -1,35 +1,29 @@
 import { yieldExecution } from '@/utils/utils'
+import { Channel } from './channel'
 import { Semaphore } from './semaphore2'
 
-type Receiver<T> = (value: T) => void
-type QueueEntry<T> = {
-    value: T
-}
-
-export class BoundedChannel<T> {
-    private queue: QueueEntry<T>[] = []
-    private receivers: Receiver<T>[] = []
+export class BoundedChannel<T> extends Channel<T> {
     private semaphores: Semaphore[] = []
 
-    constructor(private capacity: number) {}
+    constructor(private capacity: number) {
+        super()
+    }
 
     /**
      * Sends data to the channel.
      * If the channel is at maximum capacity, the sender waits until space is available.
      * Otherwise, returns immediately after sending the data.
      */
-    async send(data: T): Promise<void> {
-        if (this.queue.length >= this.capacity) {
+    override async send(data: T): Promise<void> {
+        // If the channel is full, backpressure the sender until the capacity is available
+        if (super.length >= this.capacity) {
             const semaphore = new Semaphore()
             this.semaphores.push(semaphore)
             await semaphore.wait()
         }
 
-        const receiver = this.receivers.shift()
-        receiver?.(data)
-        if (!receiver) {
-            this.queue.push({ value: data })
-        }
+        // Send data to the channel, but don't wait for it to be received
+        super.send(data)
 
         // Yield execution to allow receivers to run
         await yieldExecution()
@@ -40,25 +34,9 @@ export class BoundedChannel<T> {
      * If there is data in the queue, it is immediately returned.
      * Otherwise, the receiver waits until data is sent to the channel.
      */
-    async receive(): Promise<T> {
-        const entry = this.queue.shift()
-        if (entry) {
-            const semaphore = this.semaphores.shift()
-            semaphore?.signal()
-            return entry.value
-        } else {
-            return new Promise<T>((resolve) => {
-                this.receivers.push(resolve)
-            })
-        }
-    }
-
-    /**
-     * Returns an async generator that yields values as they are received.
-     */
-    async *iter(): AsyncGenerator<T> {
-        while (true) {
-            yield await this.receive()
-        }
+    override async receive(): Promise<T> {
+        // Signal to the oldest blocked sender that there is now capacity available
+        this.semaphores.shift()?.signal()
+        return super.receive()
     }
 }
