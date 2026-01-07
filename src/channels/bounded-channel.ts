51cced19484d1e04b8924314880da09127f81f0e
Giuseppe Lanna <49965024+King-witcher@users.noreply.github.com>
2026-01-07 20:37:04 -0300

    create the worker class

--- src/channels/bounded-channel.ts
+++ src/channels/bounded-channel.ts
@@ -1,9 +1,9 @@
+import { Signal } from '@/async/signal2'
 import { yieldExecution } from '@/utils/utils'
 import { Channel } from './channel'
-import { Semaphore } from './semaphore2'
 
 export class BoundedChannel<T> extends Channel<T> {
-    private semaphores: Semaphore[] = []
+    private signals: Signal[] = []
 
     constructor(private capacity: number) {
         super()
@@ -17,8 +17,8 @@ export class BoundedChannel<T> extends Channel<T> {
     override async send(data: T): Promise<void> {
         // If the channel is full, backpressure the sender until the capacity is available
         if (super.length >= this.capacity) {
-            const semaphore = new Semaphore()
-            this.semaphores.push(semaphore)
+            const semaphore = new Signal()
+            this.signals.push(semaphore)
             await semaphore.wait()
         }
 
@@ -36,7 +36,7 @@ export class BoundedChannel<T> extends Channel<T> {
      */
     override async receive(): Promise<T> {
         // Signal to the oldest blocked sender that there is now capacity available
-        this.semaphores.shift()?.signal()
+        this.signals.shift()?.signal()
         return super.receive()
     }
 }
---
+++ src/workers/worker.ts
@@ -0,0 +1,39 @@
+import type { Channel } from '@/channels/channel'
+
+/**
+ * A worker that processes input data concurrently and sends the results to an output channel.
+ */
+export class Worker<TInput, TOutput> {
+    constructor(
+        private output: Channel<TOutput> | Array<TOutput>,
+        private concurrency: number,
+        private workerFn: (input: TInput) => Promise<TOutput>,
+    ) {}
+
+    /**
+     * Consumes data from the source, processes it using the worker function,
+     * and sends the results to the output channel or array.
+     */
+    async consume(
+        source: Array<TInput> | Generator<TInput> | AsyncGenerator<TInput>,
+    ): Promise<void> {
+        const { resolve, reject, promise } = Promise.withResolvers<void>()
+
+        const workers = Array.from({ length: this.concurrency }).map(async () => {
+            // If source is an array, convert it to an iterator to avoid repeated processing
+            const source_ = Array.isArray(source) ? source.values() : source
+            for await (const item of source_) {
+                const result = await this.workerFn(item)
+
+                if (Array.isArray(this.output)) this.output.push(result)
+                else await this.output.send(result)
+            }
+        })
+
+        Promise.all(workers)
+            .then(() => resolve())
+            .catch(reject)
+
+        return promise
+    }
+}
